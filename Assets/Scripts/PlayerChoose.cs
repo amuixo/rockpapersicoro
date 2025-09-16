04c4a6fa451836dce5ecd2758ba2dad34abe23ef
amu <angela.c.davids@gmail.com>
2025-09-16 10:21:08 +0200

    made the choose text n disable buttons
    added it to all the buttons as well and made it so the buttons disappear when you click either one of them

--- Assets/Scripts/PlayerChoose.cs
+++ Assets/Scripts/PlayerChoose.cs
@@ -15,11 +15,18 @@ public class PlayerChoose
 //this will be the rock paper scissors that the player has chosen, -1 so its not equal to anything when you start the game
     public int playerChoose = -1;
 
-//if the rockbutton has been clicked
+//text that shows what the player has chosen
+    public TextMeshProUGUI playerChooseText;
+
+//if the rockbutton has been clicked, and disables all the buttons when its been clicked
     public void RockButton()
     {
 
     playerChoose = 0;
+    playerChooseText.text = "You chose Rock";
+    rockButton.gameObject.SetActive(false);
+    paperButton.gameObject.SetActive(false);
+    scissorsButton.gameObject.SetActive(false);
 
     }
 
@@ -27,6 +34,10 @@ public class PlayerChoose
     {
 
     playerChoose = 1;
+    playerChooseText.text = "You chose Paper";
+    rockButton.gameObject.SetActive(false);
+    paperButton.gameObject.SetActive(false);
+    scissorsButton.gameObject.SetActive(false);
 
     }
 
@@ -34,6 +45,10 @@ public class PlayerChoose
     {
 
     playerChoose = 2;
+    playerChooseText.text = "You chose Scissors";
+    rockButton.gameObject.SetActive(false);
+    paperButton.gameObject.SetActive(false);
+    scissorsButton.gameObject.SetActive(false);
 
     }
 
