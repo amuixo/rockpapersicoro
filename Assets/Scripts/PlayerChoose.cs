10219040d72d3ef2ba2d187857a557d761826d4d
amu <angela.c.davids@gmail.com>
2025-09-16 10:13:59 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerChoose.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerChoose
+{
+    //list of sprites that we want to choose (rock paper scissors)
+    public Sprite[] spritesToChooseFrom;
+
+//all the buttons
+    public Button rockButton;
+    public Button paperButton;
+    public Button scissorsButton;
+
+//this will be the rock paper scissors that the player has chosen, -1 so its not equal to anything when you start the game
+    public int playerChoose = -1;
+
+//if the rockbutton has been clicked
+    public void RockButton()
+    {
+
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# rockpapersicoro
+school project 
