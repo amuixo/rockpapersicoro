95bd1f14cd26fe6c0d729c8a66dca91309089bfc
amu <angela.c.davids@gmail.com>
2025-09-16 12:01:37 +0200

    Ai winning conditions
    added the ai and added conditions for a tie, if the player wins, and if the AI wins.

--- Assets/Scripts/AiChoose.cs
+++ Assets/Scripts/AiChoose.cs
@@ -10,13 +10,31 @@ public class AiChoose : MonoBehaviour
 
     public Image aiImage;
 
+    public PlayerChoose playerChoose;
+
+    public TextMeshProUGUI winText;
     public void PlayButton()
     {
         aiImage.gameObject.SetActive(true);
         aiChoose = Random.Range(0, sprites.Length);
-        
+
         //changes a sprite on the image component
         aiImage.sprite = sprites[aiChoose];
 
+        if(aiChoose == playerChoose.playerChoose) 
+        {
+            winText.text = "You tied!";
+            
+        }
+
+        if(aiChoose == 0 && playerChoose.playerChoose == 2 || aiChoose == 1 && playerChoose.playerChoose == 0 || aiChoose == 2 && playerChoose.playerChoose == 1)
+        {
+            winText.text = "You LOST NOOB";
+        }
+
+        if(playerChoose.playerChoose == 0 && aiChoose == 2 || playerChoose.playerChoose == 1 && aiChoose == 0 || playerChoose.playerChoose == 2 && aiChoose == 1)
+        {
+            winText.text = "You win >:)";
+        }
     }
 }
